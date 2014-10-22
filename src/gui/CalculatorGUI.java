f5fbf0d345dc7f27e524febb00809c5a38bf1a24
Pyryn <pyry.nikunen@cs.tamk.fi>
2014-10-22 11:26:34 +0300

    Adds constructor and documentation.
    Adds new default constructor that defines size of the guy window and
    sets it visible. Also adds documentation to the class with version and
    author information.

--- src/gui/CalculatorGUI.java
+++ src/gui/CalculatorGUI.java
@@ -1,5 +1,19 @@
 package gui;
 
-public class CalculatorGUI {
+import javax.swing.JFrame;
+import util.Core;
+import util.Functions;
 
+/**
+ * 
+ * @author Joonas Maliniemi, Toni Nahkala & Pyry Nikunen
+ * @version 0.1
+ * @since 1.7
+ *
+ */
+public class CalculatorGUI extends JFrame {
+	public CalculatorGUI() {
+		this.setSize(300, 400);
+		this.setVisible(true);
+	}
 }
