e059ceb7e0648471058252390a0cb1e717a4ddc8
Pyry Nikunen <pyry.nikunen@cs.tamk.fi>
2014-10-22 18:19:23 +0300

    Adds imports and implement.
    Adds import for UserInterface and makes CalculatorGUI implement UserInterface.
    
    Signed-off-by: Pyry Nikunen <pyry.nikunen@cs.tamk.fi>

--- src/gui/CalculatorGUI.java
+++ src/gui/CalculatorGUI.java
@@ -3,6 +3,7 @@ package gui;
 import javax.swing.JFrame;
 import util.Core;
 import util.Functions;
+import util.UserInterface;
 
 /**
  * 
@@ -11,7 +12,7 @@ import util.Functions;
  * @since 1.7
  *
  */
-public class CalculatorGUI extends JFrame {
+public class CalculatorGUI extends JFrame implements UserInterface {
 	public CalculatorGUI() {
 		this.setSize(300, 400);
 		this.setVisible(true);
