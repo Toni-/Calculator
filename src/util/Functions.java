06fb086b9699d5b451652b4a5f913746ff4ba10e
Toni- <toninahkala@gmail.com>
2014-10-23 12:31:45 +0300

    Adds documentation.
    Adds documentation to the functions.

--- src/util/Functions.java
+++ src/util/Functions.java
@@ -9,24 +9,44 @@ package util;
  */
 public class Functions {
 	
+	/**
+	 * @param a one of the integers given by the user
+	 * @param b one of the integers given by the user
+	 * @return the sum of the integers
+	 */
 	public int addition(int a, int b) {
 		int sum = a + b;
 		
 		return sum;
 	}
 	
+	/**
+	 * @param a one of the integers given by the user
+	 * @param b one of the integers given by the user
+	 * @return the quotient of the integers	 
+	 */
 	public int division(int a, int b) {
 		int quotient = a / b;
 		
 		return quotient;
 	}
 	
+	/**
+	 * @param a one of the integers given by the user
+	 * @param b one of the integers given by the user
+	 * @return the product of the integers	 
+	 */
 	public int multiplication(int a, int b) {
 		int product = a * b;
 		
 		return product;
 	}
 	
+	/**
+	 * @param a one of the integers given by the user
+	 * @param b one of the integers given by the user
+	 * @return the difference of the integers	 
+	 */
 	public int subtraction(int a, int b) {
 		int difference = a - b;
 		
