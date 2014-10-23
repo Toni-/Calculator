51c43d5c4effbe41267410aa4b276a150ba459db
Toni- <toninahkala@gmail.com>
2014-10-23 12:09:30 +0300

    Adds functions.
    Adds division, multiplication and subtraction functions.

--- src/util/Functions.java
+++ src/util/Functions.java
@@ -14,4 +14,22 @@ public class Functions {
 		
 		return sum;
 	}
+	
+	public int division(int a, int b) {
+		int quotient = a / b;
+		
+		return quotient;
+	}
+	
+	public int multiplication(int a, int b) {
+		int product = a * b;
+		
+		return product;
+	}
+	
+	public int subtraction(int a, int b) {
+		int difference = a - b;
+		
+		return difference;
+	}
 }
